43f3d66a7ef0bda5b7d49b106d80aaa181b1db48
Gead Kee <gangeadkee@gmail.com>
2022-10-14 23:22:36 +0800

    added RouteList + its methods

--- dstr-ass/main.h
+++ dstr-ass/main.h
@@ -1,1 +1,53 @@
 #pragma once
+
+
+template<class T> //abstract type       
+class Route
+{
+public:
+    T data;
+    Route<T>* prev;
+    Route<T>* next;
+};
+
+template<class T>
+class RouteList
+{
+    RouteLst()
+    {
+        cout << "--- Constructing the Doubly LinkedList object ---" << endl;
+        this->size = 0;
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+
+    void search_route(T route, T head, T move_right ) {
+
+    }
+
+    void iterate_II(T linked_list, T head, T move_right) {
+
+    }
+
+    void add_station(T route_data, T head, T move_right) {
+
+    }
+
+    void edit_station(T route, T route_data, T head, T move_right) {
+
+    }
+
+    void delete_station(T route, T head, T move_right) {
+
+    }
+
+}
+
+
+int search_passenger(T passengers, T passenger) {
+
+}
+
+int search_passenger_id(T arr, T length, T passenger) {
+
+}
