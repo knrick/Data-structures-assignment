cf9c2a157d39044795e8db1ab392253bffbc1d26
Dmitry Alferovn <kndrick@yandex.ru>
2022-11-02 16:16:54 +0800

    Reworked the header file.
    I think it should have all the classes and functions defined in the pseudocode.

--- dstr-ass/main.h
+++ dstr-ass/main.h
@@ -5,55 +5,85 @@
 # include <iostream>
 using namespace std;
 
-template<class T> //abstract type       
-class Route
-{
-public:
-    T data;
-    Route<T>* prev;
-    Route<T>* next;
-};
 
-template<class T>
-class RouteList
-{
-    RouteLst()
+namespace DSTR {
+    class RouteList
     {
-        cout << "--- Constructing the Doubly LinkedList object ---" << endl;
-        this->size = 0;
-        this->head = nullptr;
-        this->tail = nullptr;
-    }
+        public:
+            int id;
+            string name;
+            float prev_distance;
+            float next_distance;
+            float prev_price;
+            float next_price;
+            float prev_time;
+            float next_time;
+            RouteList* prev;
+            RouteList* next;
+        
+            RouteList();
+            
+            void search_route(RouteList *route, bool move_right);
 
-    void search_route(T route, T head, T move_right ) {
+            void iterate_ll(RouteList *linked_list, bool move_right);
 
-    }
+            void add_station(RouteList *route_data, bool move_right);
 
-    void iterate_II(T linked_list, T head, T move_right) {
+            void edit_station(RouteList *route, RouteList *route_data, bool move_right);
 
-    }
+            void delete_station(RouteList *route, bool move_right);
 
-    void add_station(T route_data, T head, T move_right) {
+            float* calculate_path(string start_station_name, string end_station_name, bool move_right);
 
-    }
+    };
 
-    void edit_station(T route, T route_data, T head, T move_right) {
+    class Transaction {
+        public:
+            int id;
+            int ticket_id;
+            std::string source_station;
+            std::string target_station;
+            float price;
+            time_t purchase_date_time;
+            time_t departure_date_time;
+            std::string passenger_name;
+        
+            Transaction();
 
-    }
+    };
 
-    void delete_station(T route, T head, T move_right) {
+    class TransactionQueue {
+        private:
+            Transaction *transactions;
+        public:
+            TransactionQueue();
 
-    }
+            void push(Transaction transaction);
 
+            void add_transaction(Transaction transaction);
+
+            void edit_transaction(int transaction_id, Transaction new_transaction);
+
+            void delete_transaction(int transaction_id);
+    };
+
+    class Passenger {
+        public:
+            int id;
+            std::string name;
+            std::string ic;
+            TransactionQueue* transactions;
+
+            Passenger();
+
+            int search_passenger(Passenger passenger);
+
+            int search_passenger_id(int length, Passenger passenger);
+
+            void add_passenger(Passenger passenger);
+
+    };
 }
 
-
-int search_passenger(T passengers, T passenger) {
-
-}
-
-int search_passenger_id(T arr, T length, T passenger) {
-
-}
 
 #endif
