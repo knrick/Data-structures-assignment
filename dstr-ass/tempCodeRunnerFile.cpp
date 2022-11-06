61036c39e20e93583928090bfc7139f4bdf70dc3
yuuu76 <yuwq01@gmail.com>
2022-11-06 17:29:52 +0800

    add transactions

---
+++ dstr-ass/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+    // TransactionQueue *queue = new TransactionQueue();
+    // Transaction *transaction = new Transaction(
+    //     2,
+    //     "Titiwangsa",
+    //     "Majlis Jamek",
+    //     2.0,
+    //     "2:00",
+    //     "2:15",
+    //     "Customer"
+    // );
